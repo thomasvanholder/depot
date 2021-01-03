8f6302655aae667817cbdffe5b07e7588b2ae968
Thomas Van Holder <thomasvanholder@hey.com>
2021-01-03 21:09:50 +0000

    controller tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,6 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true, length: { minimum: 10 }, format : {
-
-  }
+  validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
