bbaca4358407be42b6dd0e35022efe7ecca1e421
Thomas Van Holder <thomasvanholder@hey.com>
2021-01-03 20:40:03 +0000

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10 }, format : {
+
+  }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
