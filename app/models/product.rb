f87a692a72916da0339cfd7fe75cc86577067bb4
Thomas Van Holder <thomasvanholder@hey.com>
2021-01-04 09:50:45 +0000

    test added for cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,15 @@ class Product < ApplicationRecord
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item #hook method
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
+
 end
