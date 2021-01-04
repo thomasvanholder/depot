3cd87461f6941eadc1750f24b10d60574e180f30
Thomas Van Holder <thomasvanholder@hey.com>
2021-01-04 09:34:45 +0000

    module cart added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_175306) do
+ActiveRecord::Schema.define(version: 2021_01_04_090607) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
